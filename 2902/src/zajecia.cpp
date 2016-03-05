55e9dd256e758d49f3ef882b10a760950fe9b9e6
Patryk Pawlus <218643@student.pwr.edu.pl>
2016-03-05 11:23:14 +0100

    Liczenie czasu

--- 2902/src/zajecia.cpp
+++ 2902/src/zajecia.cpp
@@ -1,37 +1,49 @@
 #include <cstdlib>
 #include <iostream>
-#include "zajecia.
-
+#include <string>
+#include <fstream>
+#include <time.h>
 
 using namespace std;
 
-class tablica;
-{
+class Tablica {
+private:
+    int *tablica;
 
 public:
-    int zwieksztab() {
-        for (wymiar=0;wymiar<n; wymiar++)
-        tab[wymiar]=new int *[wymiar];
+    int dodajElementy(int n){
+        tablica= new int [n];{
+        for (int i=0;i<n,i++);
+        tablica[n]=rand();
     }
+ }
+    void zwolnijPamiec(){
+        delete [] tablica;
+        }
 
+};
 
-    bool pelna() {
+clock_t start, stop;
+double czas;
 
+int main()
+{
+/*
+n[5]={10^1,10^3,10^5,10^6,10^9};
+*/
+start=clock();
+for (int i=0;i<5;i++);
+Tablica tablica;
+int ile=5;
+tablica.dodajElementy(ile);
+tablica.zwolnijPamiec();
 
-    }
-
-
-    bool zapisz() {
-
-
-    }
-
-private:
-
-
-
-
-
-
+stop=clock();
+czas= (double)(stop-start);
+cout<<"Czas zapisu(przy zwiekszaniu o jeden):"<<czas<<endl;
+return 0;
 
 }
+
+
+
