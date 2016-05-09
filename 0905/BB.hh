44615fddb057c1e583675efe1ea9f943ebb76b8e
Patryk Pawlus <218643@student.pwr.edu.pl>
2016-05-09 12:12:23 +0200

    Praca na zajeciach, nie udalo sie zbyt wiele zrobic...

---
+++ 0905/BB.hh
@@ -0,0 +1,26 @@
+#include "graph.hh"
+#include "lista.hh"
+#include "graphsearch.hh"
+
+
+
+
+/*!
+*
+*
+*
+*/
+class BranchAndBound
+{
+
+private:
+    int ways; //zmienna prechowujaca ilosc sciezek.
+public:
+    void BranchAndBoundMetod();
+
+
+
+
+
+
+};
